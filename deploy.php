badcf214db00add1791db6def79325902062b40c
kop <matija.kop1@gmail.com>
2021-04-30 11:15:09 +0200

    wip

--- deploy.php
+++ deploy.php
@@ -9,9 +9,7 @@ require 'recipe/slack.php';
 
 
 set('slack_webhook', 'https://hooks.slack.com/services/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');
-set('slack_text', '_{{user}}_ deploying `{{branch}}` to *{{target}}*');
-set('slack_success_text', 'Deploy to *{{target}}* successful');
-set('slack_failure_text', 'Deploy to *{{target}}* failed');
+before('deploy', 'slack:notify');
 
 set('application', 'My App');
 set('ssh_multiplexing', true); // Speeds up deployments
@@ -42,24 +40,26 @@ task('deploy:secrets', function () {
     upload('.env', get('deploy_path') . '/shared');
 });
 
-before('deploy', 'slack:notify');
+// Production Server
+host('myapp.io') // Name of the server
+->hostname('104.248.172.220') // Hostname or IP address
+->stage('production') // Deployment stage (production, staging, etc)
+->user('root') // SSH user
+->set('deploy_path', '/var/www/my-app'); // Deploy path
 
 // Staging Server
-host('laravel-deployer.notus.dev') // Name of the server
+host(['laravel-deployer-test.notus.dev']) // Name of the server
 ->hostname('5.189.130.105') // Hostname or IP address
 ->stage('staging') // Deployment stage (production, staging, etc)
 ->user('root') // SSH user
 ->set('deploy_path', '/var/www/vhosts/notus.dev/laravel-deployer-test.notus.dev'); // Deploy path
 
-task('deploy:hello', function () {
-    run('ls -la');
-});
-
 after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
-after('success', 'slack:notify:success');
 
 desc('Deploy the application');
+
 task('deploy', [
+    'deploy:info',
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
@@ -71,11 +71,10 @@ task('deploy', [
     'artisan:storage:link', // |
     'artisan:view:cache',   // |
     'artisan:config:cache', // | Laravel Specific steps
-    //'artisan:optimize',     // |
+    'artisan:optimize',     // |
     'artisan:migrate',      // |
     'artisan:queue:restart',// |
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
 ]);
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test
+                    Laravel Test 1
                 </div>
 
             </div>
